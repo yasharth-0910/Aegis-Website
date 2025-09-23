b023dfc39abf997dd372f1bec003860f2c2ce788
Yasharth Singh <yasharthsingh0910@gmail.com>
2025-09-24 00:50:27 +0530

    intial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // ✅ Disables ESLint during `next build`
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
